8a83ef933aabe828ae5f55fa8c53b197f89c1291
wKhanHere <wak4746@gmail.com>
2025-09-19 23:53:07 +0530

    Add files via upload

---
+++ RRPG (Retro RPG)/RRPG (Retro RPG)/rpg.py
@@ -0,0 +1,9 @@
+import random
+import copy
+import GridStructure
+import object_creation
+import LivingEntities
+
+class Battle():
+    def __init__(self.MobPool:list,self.LootPool:list): #Add a default mobpool and lootpool.
+        pass
