083ceca55ba90590908b8b54ff3e4984559cb19b
Waqar Ahmad Khan <wak4746@gmail.com>
2025-11-11 23:02:40 +0530

    Changed file path

--- RRPG/__init__.py
+++ RRPG/__init__.py
@@ -1,2 +1,2 @@
 import runpy
-runpy.run_path(r"c:\Users\khan6\OneDrive\Desktop\Python\RRPG (Retro RPG)\rpg.py", run_name="__main__")
+runpy.run_path(r"c:\Users\khan6\OneDrive\Desktop\Python\RRPG\rpg.py", run_name="__main__")
